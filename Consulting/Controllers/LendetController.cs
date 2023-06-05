bdb7a7648898fe8309bb86da24096cd15cf6b065
rinor.mehmeti <rinor.mehmeti@LIRIMISLAMI>
2023-06-05 16:55:44 +0200

    continue

--- Consulting/Controllers/LendetController.cs
+++ Consulting/Controllers/LendetController.cs
@@ -1,6 +1,7 @@
 ﻿using Consulting.Data;
 using Consulting.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace Consulting.Controllers
 {
@@ -9,11 +10,11 @@ namespace Consulting.Controllers
         private readonly ApplicationDbContext _db;
         public LendetController(ApplicationDbContext db)
         {
-            _db= db;
+            _db = db;
         }
         public IActionResult Index()
         {
-            List<Lendet> LendetList = _db.Lendet.ToList(); 
+            List<Lendet> LendetList = _db.Lendet.ToList();
             return View(LendetList);
         }
         [HttpGet]
@@ -24,14 +25,73 @@ namespace Consulting.Controllers
         [HttpPost]
         public IActionResult Create(Lendet obj)
         {
-             if (ModelState.IsValid)
+            //if (obj.Lname == obj.Ldescription.ToString())
+            //{
+            //    ModelState.AddModelError("Emri", "Emri i lendes dhe pershkrimi nuk mund te jene te njejta");
+            //}
+            if (ModelState.IsValid)
             {
                 _db.Lendet.Add(obj);
                 _db.SaveChanges();
                 return RedirectToAction("Index");
 
             }
-             return View();
+            return View();
+        }
+        public IActionResult Edit(int? Lid)
+        {
+            if (Lid == null || Lid == 0)
+            {
+                return NotFound();
+            }
+            Lendet? lendetprejdb = _db.Lendet.Find(Lid);
+            //Lendet? lendetprejdb1 = _db.Lendet.FirstOrDefault(u=>u.Lid==Lid);
+            //Lendet? lendetprejdb2 = _db.Lendet.Where(u=>u.Lid == Lid).FirstOrDefault();
+            if (lendetprejdb == null) 
+            {
+                return NotFound(); 
+            }
+            return View(lendetprejdb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Lendet obj)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _db.Lendet.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            return View();
+        }
+        public IActionResult Delete(int? Lid)
+        {
+            if (Lid == null || Lid == 0)
+            {
+                return NotFound();
+            }
+            Lendet? lendetprejdb = _db.Lendet.Find(Lid);
+            //Lendet? lendetprejdb1 = _db.Lendet.FirstOrDefault(u=>u.Lid==Lid);
+            //Lendet? lendetprejdb2 = _db.Lendet.Where(u=>u.Lid == Lid).FirstOrDefault();
+            if (lendetprejdb == null)
+            {
+                return NotFound();
+            }
+            return View(lendetprejdb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? Lid)
+        {
+            Lendet? obj =_db.Lendet.Find(Lid);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Lendet.Remove(obj); 
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
